83d9003715deadcfbd1c6b05de190096827cb861
emster7013 <52601501+emster7013@users.noreply.github.com>
2019-11-08 20:43:17 -0500

    tabs done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topsDiv = document.querySelector(".topics");
+
+// Asynchronous Function
+EmTopics = async url => {
+  try {
+    response = await axios.get(url);
+    response.data.topics.forEach(item => {
+      topsDiv.appendChild(topsTabs(item));
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+EmTopics("https://lambda-times-backend.herokuapp.com/topics");
+
+
+function topsTabs(obj) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = obj;
+
+  return tab;
+}
